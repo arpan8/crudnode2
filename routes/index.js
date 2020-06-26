0635174de44022431e8919410cbf671dd4283ec7
arpan <arpan@wrctpl.com>
2020-06-26 22:27:41 +0530

    committed by Arpan

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,53 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../models').user;
-var State = require('../models').states;
-var City = require('../models').city;
+const express = require('express');
+const router = express.Router();
+const User = require('../models').user;
+const State = require('../models').states;
+const City = require('../models').city;
+const gravatar = require('gravatar');
+const multer = require('multer');
+var picture_storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'public/images');
+  },
+  filename: function(req, file, cb){
+    fileExt = file.mimetype.split("/").pop();
+    if(fileExt == 'jpg' || fileExt == 'jpeg' || fileExt == 'png'){
+      fileName = file.fieldname + "-" + Date.now() +"." + fileExt;
+    }
+    cb(null, fileName);
+  }
+});
+var picture = multer({
+  storage: picture_storage
+});
 /* GET home page. */
 router.get('/', async(req, res)=> {
   var state = await State.findAll({
     attributes: ['id','name']
   });
+  User.belongsTo(State,{
+    foreignKey: 'states'
+  });
+  User.belongsTo(City,{
+    foreignKey: 'cities'
+  });
+  var user = await User.findAll({
+    include:[{
+        model: State,
+        attributes:['id','name']
+    },{
+        model: City,
+        attributes:['id','name']
+      }
+    ],
+    attributes: ['id','first_name','last_name','email','mobile_no','picture']
+  });
   res.render('frontend/index',{
-    state
+    state,
+    user
   });
 });
+
 router.post('/get-states', async(req,res)=>{
   var state = await State.create({
     name: req.body.name,
@@ -22,6 +58,7 @@ router.post('/get-states', async(req,res)=>{
     state
   });
 });
+
 router.post('/cities', async(req,res)=>{
   var city = await City.create({
     name: req.body.name,
@@ -33,6 +70,7 @@ router.post('/cities', async(req,res)=>{
     city
   });
 });
+
 router.post('/get-cities', async(req,res)=>{
   var city = await City.findAll({
     where: {
@@ -45,14 +83,48 @@ router.post('/get-cities', async(req,res)=>{
     city
   });
 });
-router.post('/user-registration', async(req, res)=>{
+
+router.post('/user-registration',picture.single('picture'), async(req, res)=>{
+  const avatar =gravatar.url(req.body.email,{
+    s: '150',
+    r: 'pg',
+    d: 'mm'
+  });
   var user = await User.create({
     first_name: req.body.firstname,
     last_name: req.body.lastname,
     email: req.body.email,
     password: req.body.password,
     mobile_no: req.body.mobile_no,
-      
-  })
-})
+    picture: (req.file === undefined) ? avatar : `/images/${req.file.filename}`,
+    states: req.body.state,
+    cities: req.body.cities
+  });
+
+  User.belongsTo(State,{
+    foreignKey: 'states'
+  });
+  User.belongsTo(City,{
+    foreignKey: 'cities'
+  });
+  var user_details = await User.findOne({
+    where: {
+      id: user.id
+    },include:[{
+        model: State,
+        attributes:['id','name']
+    },{
+        model: City,
+        attributes:['id','name']
+      }
+    ],
+    attributes: ['id','first_name','last_name','email','mobile_no','picture']
+  });
+  res.json({
+    success: true,
+    code: 200,
+    user,
+    user_details
+  });
+});
 module.exports = router;
